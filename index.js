b34b95bdb98c3408666288048756b2a0c7399c53
jonschlinkert <jon.schlinkert@sellside.com>
2015-11-29 07:06:19 -0500

    first commit.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * base-routes <https://github.com/jonschlinkert/base-routes>
+ *
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
